0090d0b957f0a1b17161163fd6974fc6bd118c41
Abdur Rahman <abdur1222.ar@gmail.com>
2023-10-18 00:20:25 +0600

    Created a Router and PrivateRoute for enabled client side

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Layout/Root";
+import ErrorPage from "../Pages/ErrorPage";
+import Home from "../Pages/Home";
+import PrivateRoute from "./PrivateRoute";
+import AddProducts from "../Pages/AddProducts";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/card/:id",
+        element: (
+          <PrivateRoute>
+            <AddProducts></AddProducts>
+          </PrivateRoute>
+        ),
+        loader: () => fetch('/categories.json')
+      },
+    ],
+  },
+]);
+
+export default router;
